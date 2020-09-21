c729863c5bb6ea54fdcc8a5c34c5ad2de6fbb248
ryoma116 <uma19880116>
2020-09-21 21:21:31 +0900

    new update view

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from todo.views import TodoListView, TodoDetailView, TodoCreateView, TodoDeleteView
+from todo.views import TodoListView, TodoDetailView, TodoCreateView, TodoDeleteView, TodoUpdateView
 
 urlpatterns = [
     path('list/', TodoListView.as_view(), name='list'),
     path('detail/<int:pk>', TodoDetailView.as_view(), name='detail'),
     path('create/', TodoCreateView.as_view(), name='create'),
     path('delete/<int:pk>', TodoDeleteView.as_view(), name='delete'),
+    path('update/<int:pk>', TodoUpdateView.as_view(), name='update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from todo.models import TodoModel
 
@@ -16,6 +16,13 @@ class TodoDetailView(DetailView):
 
 class TodoCreateView(CreateView):
     template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
+
+
+class TodoUpdateView(UpdateView):
+    template_name = 'update.html'
     model = TodoModel
     fields = ('title', 'memo', 'priority', 'duedate')
     success_url = reverse_lazy('list')
