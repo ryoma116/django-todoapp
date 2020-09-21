29e006f43ffea019f83aaa2ef2d8bc9169c1d07a
ryoma116 <uma19880116>
2020-09-21 11:20:48 +0900

    new detail view

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from todo.views import TodoListView
+from todo.views import TodoListView, TodoDetailView
 
 urlpatterns = [
     path('list/', TodoListView.as_view()),
+    path('detail/<int:pk>', TodoDetailView.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 from todo.models import TodoModel
 
 
 class TodoListView(ListView):
     template_name = 'list.html'
+    model = TodoModel
+
+
+class TodoDetailView(DetailView):
+    template_name = 'detail.html'
     model = TodoModel
